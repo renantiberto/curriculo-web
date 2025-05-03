c2e4e4acacfc2f051f22258c7abd1373f8a67306
Renan Tiberto <renan.tiberto@gmail.com>
2025-05-03 17:51:57 -0300

    Update Curriculo Web
    Add Dynamic Experience Time

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const btnDownloadCv = document.getElementById("download_cv")
+const btnDownloadCv = document.getElementById("download_cv");
+const admissionDate = new Date("2021-12-20");
 
 btnDownloadCv.addEventListener("click", () => {
     //const cv = document.getElementById("app")
@@ -23,3 +24,23 @@ btnDownloadCv.addEventListener("click", () => {
 
     html2pdf().set(options).from(cv).save();
 })
+
+function experienceTime( referenceDate ) {
+    const currentDate = new Date();
+    const fixedDate = new Date( referenceDate );
+
+    let years = currentDate.getFullYear() - fixedDate.getFullYear();
+
+    const currentMounth = currentDate.getMonth();
+    const currentDay = currentDate.getDate();
+    const fixedMounth = fixedDate.getMonth();
+    const fixedDay = fixedDate.getDate();
+
+    if ( currentMounth < fixedMounth || (currentMounth === fixedMounth && currentDay < fixedDay)) {
+        years--;
+    }
+
+    return years;
+}
+
+document.getElementById("time-exp").textContent = experienceTime( admissionDate );
